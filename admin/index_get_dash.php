14f2cb53a026251975eeef48c77aed303b0ad2dc
cokeIII <coke2484@gmail.com>
2022-08-31 13:35:33 +0700

    f

--- admin/index_get_dash.php
+++ admin/index_get_dash.php
@@ -1,51 +1,69 @@
-<?php 
+<?php
 require_once "connect.php";
-function getCourse_dash() {
+function getCourse_dash()
+{
     global $conn;
     $sql = "select count(course_id) as numCourse from course";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     return $row["numCourse"];
 }
-function getUsers_dash() {
+function getUsers_dash()
+{
     global $conn;
     $sql = "select count(id_card) as numUsers from users";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     return $row["numUsers"];
 }
-function getUsers_regis_dash_per() {
+function getUsers_regis_dash_per()
+{
     global $conn;
     $sql = "select * 
     from users u
     inner join course_regis cr on cr.id_card = u.id_card
     group by cr.id_card
     ";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     // $row = mysqli_fetch_array($res);
     $num = mysqli_num_rows($res);
-    return $num*100/getUsers_dash();
+    return $num * 100 / getUsers_dash();
 }
-function getUsers_regis_dash() {
+function getUsers_regis_dash()
+{
     global $conn;
     $sql = "select * 
     from users u
     inner join course_regis cr on cr.id_card = u.id_card
     group by cr.id_card
     ";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     // $row = mysqli_fetch_array($res);
     $num = mysqli_num_rows($res);
     return $num;
 }
-function getUsers_success_dash() {
+function getUsers_success_dash()
+{
     global $conn;
     $sql = "select count(cr.id_card) as numUsersSucc 
     from users u
     inner join course_regis cr on cr.id_card = u.id_card
     where cr.status = 'pass' group by cr.id_card
     ";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
     return $row["numUsersSucc"];
+}
+if (!empty($_POST)) {
+    if ($_POST["act"] == "getAreaChart") {
+        $sql = "select count(id_card) as numMonth, EXTRACT(MONTH FROM time_stamp) as mounts from course_regis
+        GROUP BY  EXTRACT(MONTH FROM time_stamp)
+        ";
+        $data = array();
+        $res = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_array($res)){
+            $data[$row["mounts"]] = $row["numMonth"];
+        }
+        echo json_encode($data);
+    }
 }
